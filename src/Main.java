999502c5837b0c297a66f7769d599873c11d21a3
AnQiYAN <781236437@qq.com>
2021-12-30 15:59:16 +0800

    added code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello world");
+        int a = 5;
+        int b = 6;
+        if (a > b){
+            System.out.println("a is larger than b");
+        }else{
+            System.out.println("A is smaller than b");
+        }
     }
 }
